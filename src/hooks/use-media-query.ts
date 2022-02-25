6bce56b85f4cd2ead7a1d08f5a7fccab33915db8
Illia Chaban <illia.chaban.8@gmail.com>
2022-02-25 14:03:06 -0500

    update solid styled components package; remove logs"

--- src/hooks/use-media-query.ts
+++ src/hooks/use-media-query.ts
@@ -2,7 +2,6 @@ import { Accessor, onCleanup } from 'solid-js'
 import { useAtom } from './use-atom'
 
 export const useMediaQuery = (...queries: string[]): Accessor<boolean> => {
-  console.log('use media query')
   const query = queries.join(' and ')
 
   const media = window.matchMedia(query)
