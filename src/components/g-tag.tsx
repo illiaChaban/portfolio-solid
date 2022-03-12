7cf2bf091a9f126f0ca40ba6349c4b0cd448af80
Illia Chaban <illia.chaban.8@gmail.com>
2022-03-12 12:08:19 -0500

    udpate gtag to not break prod

--- src/components/g-tag.tsx
+++ src/components/g-tag.tsx
@@ -1,23 +1,21 @@
-import { onMount } from "solid-js";
-import { JSX } from "solid-js/jsx-runtime";
-import { loadScript } from "../utils/load-script";
+import { Component, onMount } from 'solid-js'
+import { loadScript } from '../utils/load-script'
 
-// TODO: Add lodash once
-export const GTag = (): JSX.Element => {
-  onMount( async () => {
+export const GTag: Component = p => {
+  onMount(async () => {
     const id = 'UA-178460557-1'
 
     await loadScript(`https://www.googletagmanager.com/gtag/js?id=${id}`)
-    
+
     const w = window as any
-    w.dataLayer = w.dataLayer || [];
+    w.dataLayer = w.dataLayer || []
     const dataLayer = w.dataLayer
 
-    function gtag(...args: any[]){
-      dataLayer.push(args);
+    function gtag(...args: any[]) {
+      dataLayer.push(args)
     }
     function gtagEvent(eventName: any, config: any) {
-      gtag('event', eventName, config);
+      gtag('event', eventName, config)
     }
     function gtagSetPath() {
       gtagEvent('page_view', {
@@ -25,9 +23,9 @@ export const GTag = (): JSX.Element => {
       })
     }
 
-    gtag('js', new Date());
-    gtag('config', id);
-    gtagSetPath();
+    gtag('js', new Date())
+    gtag('config', id)
+    gtagSetPath()
   })
-  return
+  return <>{p.children}</>
 }
