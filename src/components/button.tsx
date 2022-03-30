727a96a0c4c93780e761ca8c5a497e6e015a623e
Illia Chaban <illia.chaban.8@gmail.com>
2022-03-30 02:00:01 -0400

    fix button hover on window scroll

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,11 @@
 import { LinkProps } from 'solid-app-router'
-import { createMemo, createRoot, createSignal, JSX } from 'solid-js'
+import { Accessor, createMemo, createRoot, createSignal, JSX } from 'solid-js'
 import { PageLinkBase, PageLinkBaseProps } from '.'
 import { use, useRect } from '../hooks'
 import { css, keyframes, makeStyles, styled } from '../theme'
 import { OmitSafe, Page } from '../types'
 import { bindEventWithCleanup, cx, throttle, withActions } from '../utils'
-import { has, minMax } from '../utils/lodash'
+import { has, minMax, pick } from '../utils/lodash'
 
 const useStyles = makeStyles()({
   btn: ({ colors }) => css`
@@ -37,11 +37,9 @@ const useStyles = makeStyles()({
     &:focus {
       box-shadow: 0 0 20px 1px var(--btn-color);
     }
-
-    &:hover {
-      color: ${colors.background};
-      /* font-weight: 900;  */ /* changes btn width on firefox */
-    }
+  `,
+  hover: ({ colors }) => css`
+    color: ${colors.background};
   `,
 })
 
@@ -58,20 +56,16 @@ type ButtonProps = {
     }
 )
 export const Button = (p: ButtonProps): JSX.Element => {
-  const mousePosition$ = withActions(
-    createSignal<Point>({ x: 0, y: 0 }),
-    set => ({
-      track: throttle(10, (e: MouseEvent) => {
-        set({ x: e.pageX, y: e.pageY })
-      }),
-    }),
-  )
+  const mousePosition$ = useMousePosition()
 
   const rect$ = useRect()
   const gradient$ = createMemo(
-    (): Record<'x' | 'y' | 'opacity' | 'from' | 'to', number> => {
+    (): Record<'x' | 'y' | 'opacity' | 'from' | 'to', number> & {
+      isHovering: boolean
+    } => {
       const rect = rect$()
-      if (!rect) return { x: 0, y: 0, opacity: 0, from: 0, to: 0 }
+      if (!rect)
+        return { x: 0, y: 0, opacity: 0, from: 0, to: 0, isHovering: false }
       const { x, y } = mousePosition$()
       const { left, right, top, bottom } = rect
       const width = right - left
@@ -104,6 +98,7 @@ export const Button = (p: ButtonProps): JSX.Element => {
         opacity: closeness === 1 ? 1 : closeness * 0.5,
         from: closeness === 1 ? 0.5 : closeness * 0.1,
         to: closeness === 1 ? 1 : minMax(0.3, 0.7)(closeness),
+        isHovering: closeness === 1,
       }
     },
   )
@@ -116,13 +111,17 @@ export const Button = (p: ButtonProps): JSX.Element => {
 
   const styles = useStyles()
 
-  bindEventWithCleanup(window, 'mousemove', mousePosition$.track)
-
   return (
     <>
       <Component
         ref={use(rect$.track, ripple)}
-        class={cx(styles.btn(), p.class)}
+        class={cx(
+          styles.btn(),
+          // using hover detected with JS, because CSS doesn't react to scroll
+          // even when mouse is over the button
+          gradient$().isHovering && styles.hover(),
+          p.class,
+        )}
         onClick={p.onClick as any}
       >
         <Backdrop
@@ -139,6 +138,28 @@ export const Button = (p: ButtonProps): JSX.Element => {
       </Component>
     </>
   )
+}
+
+type MousePosition = Point & { scrollY: number }
+const useMousePosition = (): Accessor<Point> => {
+  const mousePosition$ = withActions(
+    createSignal<MousePosition>({ x: 0, y: 0, scrollY: window.scrollY }),
+    set => ({
+      track: throttle(10, (e: MouseEvent) => {
+        set({ x: e.pageX, y: e.pageY, scrollY: window.scrollY })
+      }),
+      trackScroll: throttle(10, () => {
+        set(({ x, y, scrollY }) => {
+          const scrollDiff = window.scrollY - scrollY
+          return { x, y: y + scrollDiff, scrollY: window.scrollY }
+        })
+      }),
+    }),
+  )
+  bindEventWithCleanup(window, 'mousemove', mousePosition$.track)
+
+  bindEventWithCleanup(window, 'scroll', mousePosition$.trackScroll)
+  return () => pick(mousePosition$(), ['x', 'y'])
 }
 
 const Backdrop = styled('div')`
