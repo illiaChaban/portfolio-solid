bd00878bf0a7fd8877cdacf13a8e64be575adb86
Illia Chaban <illia.chaban.8@gmail.com>
2023-01-12 12:30:00 -0500

    remove fade in transition for now

--- src/components/page-transition/fade-transition.tsx
+++ src/components/page-transition/fade-transition.tsx
@@ -4,25 +4,27 @@ import { Transition } from 'solid-transition-group'
 const time = 250
 
 export const FadeInTransition = (p: { children: JSX.Element }) => {
-  return (
-    <Transition
-      onEnter={(el, done) => {
-        // Animating fade in on enter results in weird white background
-        setTimeout(done, time)
-      }}
-      onExit={(el, done) => {
-        window.scroll(0, 0)
-        el.animate(
-          { opacity: ['1', '0'] },
-          {
-            duration: time,
-            easing: 'ease-out',
-            fill: 'forwards',
-          },
-        ).finished.then(done)
-      }}
-    >
-      {p.children}
-    </Transition>
-  )
+  return <>{p.children}</>
+  // TODO: fixme
+  // return (
+  //   <Transition
+  //     onEnter={(el, done) => {
+  //       // Animating fade in on enter results in weird white background
+  //       setTimeout(done, time)
+  //     }}
+  //     onExit={(el, done) => {
+  //       window.scroll(0, 0)
+  //       el.animate(
+  //         { opacity: ['1', '0'] },
+  //         {
+  //           duration: time,
+  //           easing: 'ease-out',
+  //           fill: 'forwards',
+  //         },
+  //       ).finished.then(done)
+  //     }}
+  //   >
+  //     {p.children}
+  //   </Transition>
+  // )
 }
