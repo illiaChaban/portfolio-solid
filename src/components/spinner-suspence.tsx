0d44345eef0dabb9125369e7d3bf2421ea800965
Illia Chaban <illia.chaban.8@gmail.com>
2023-01-14 01:52:22 -0500

    fix transition spinner for about art

--- src/components/spinner-suspence.tsx
+++ src/components/spinner-suspence.tsx
@@ -1,27 +1,40 @@
 import { JSX, Suspense } from 'solid-js'
 import { Transition } from 'solid-transition-group'
-import { flow } from '../utils'
+import { css } from '../theme'
+import { bindEvent } from '../utils'
 import { BlobSpinner } from './blob-spinner'
 
 export const SpinnerSuspence = (p: { children?: JSX.Element }) => {
-  const getElOpacity = flow(getComputedStyle, ({ opacity }) =>
-    Number(opacity || '1'),
-  )
-
   const spinnerId = 'spinner'
   const isSpinner = (el: Element) => el.getAttribute('data-id') === spinnerId
 
+  const hideCss = css`
+    opacity: 0 !important;
+  `
+  const transitionCss = css`
+    transition: opacity 200ms;
+  `
+
   return (
     <Transition
+      onBeforeEnter={el => {
+        el.classList.add(hideCss, transitionCss)
+      }}
       onEnter={(el, done) => {
-        el.animate([{ opacity: 0 }, { opacity: getElOpacity(el) }], {
-          duration: 200,
-        }).finished.then(done)
+        el.classList.remove(hideCss)
+        const cleanup = bindEvent(el, 'transitionend', () => {
+          done()
+          el.classList.remove(transitionCss)
+          cleanup()
+        })
       }}
       onExit={(el, done) => {
-        el.animate([{ opacity: getElOpacity(el) }, { opacity: 0 }], {
-          duration: isSpinner(el) ? 400 : 200,
-        }).finished.then(done)
+        el.animate(
+          { opacity: 0 },
+          {
+            duration: isSpinner(el) ? 400 : 200,
+          },
+        ).finished.then(done)
       }}
     >
       <Suspense fallback={<BlobSpinner data-id={spinnerId} />}>
